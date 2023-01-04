bce26da2ba19b6340c9efc5e49fdee5f466cfbaa
NatJoshYoyo <joshuaobrien28@yahoo.com>
2023-01-04 00:56:51 -0500

    Added local Storage

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
 
 
-
 var searchBtn = document.getElementById("searchBtn");
 var forecastEl = document.getElementById("forecasts");
+
+var historyArray = localHistory();
 
 function convertSearch() {
     var input = document.getElementById("input").value.trim();
@@ -14,64 +15,121 @@ function convertSearch() {
         var lat = data[0].lat;
         var city = data[0].name;
         getWeatherData(lon, lat, city);
-        saveHistory(input);
+        saveHistory(city);
+        updateHistory();
+        document.getElementById("input").value = "";
+    })
+};
+
+function convertHistorySearch(historyBtnInfo) {
+    var input = historyBtnInfo.textContent;
+
+    fetch("https://api.openweathermap.org/data/2.5/weather?" + lat + "&lon=" + lon + "&exclude=minutely&units=imperial&appid=4c68348ebef06f62bb4687ae1b234b42")
+    .then((response) => response.json())
+    .then(function (data) {
+        var lon = data[0].lon;
+        var lat = data[0].lat;
+        var city = data[0].name;
+        getWeatherData(lon, lat, city);
     })
 };
 
 function getWeatherData(lon, lat, city) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?" + lat + "&lon=" + lon + "&exclude=minutely&units=imperial&appid=4c68348ebef06f62bb4687ae1b234b42")
-    .then((response) => response.json())
-    .then(function(data) {
-        console.log(data);
-        currentForecast(data, city);
-        weekOut(data);
-    })
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely&units=imperial&appid=3235f6ca43f152b21beee3053909231f")
+        .then((response) => response.json())
+        .then(function (data) {
+            console.log(data);
+            currentDay(data, city);
+            fiveDay(data);
+        })
 }
 
-function currentDate(data, city) {
-    var date = new Date((data.current.dt*1000)-(data.timezone_offset*1000))
+//Current Weather
+function currentDay(data, city) {
+    var date = new Date((data.current.dt * 1000) - (data.timezone_offset * 1000))
     var icon = data.current.weather[0].icon;
 
-    var currentLocationEl = document.getElementById("currentLocation");
+    var currentyCityEl = document.getElementById("currentCity");
     var currentDateEl = document.getElementById("currentDate");
     var currentIconEl = document.getElementById("currentIcon");
     var currentTempEl = document.getElementById("currentTemp");
     var currentHumidityEl = document.getElementById("currentHumidity");
     var currentWindEl = document.getElementById("currentWind");
 
-    currentLocationyEl.textContent = city;
-    currentDayEl.textContent = data.toLocaleDateString("en-Us");
-    currentIconEl.setAttribute("src","https://openweathermap.org/img/wn/"+ icon + "@2xpng");
-    currentHumidityEl.textContent = data.current.humidty;
+    currentyCityEl.textContent = city;
+    currentDateEl.textContent = date.toLocaleDateString("en-US");
+    currentIconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + icon + "@2x.png");
+    currentTempEl.textContent = data.current.temp;
+    currentHumidityEl.textContent = data.current.humidity;
     currentWindEl.textContent = data.current.wind_speed;
 }
 
+//Forecast week out
 function weekOut(data) {
 
     for (var i = 1; i < 6; i++) {
-        var date = new Date((data.daily[i].dt*1000)-(data.timezone_offset*1000));
+        var date = new Date((data.daily[i].dt * 1000) - (data.timezone_offset * 1000));
         var icon = data.daily[i].weather[0].icon;
 
-    var dateEl = document.getElementById("data" + i);
-    var iconEl = document.getElementById("icon" + i);
-    var tempEl = document.getElementById("temp" + i);
-    var humidityEl = document.getElementById("humidity" + i);
-    var windEl = document.getElementById("humidity" + i);
-    var windEl = document.getElementById("wind" + i);
+        var dateEl = document.getElementById("date" + i);
+        var iconEl = document.getElementById("icon" + i);
+        var tempEl = document.getElementById("temp" + i);
+        var humidityEl = document.getElementById("humidity" + i);
+        var windEl = document.getElementById("wind" + i);
 
-    dateEl.textContent = data.toLocaleDateString("en-US");
-    iconEl.setAttribute("src", "https://openweathermap.org/img/wn/"+ icon + "@2.png");
-    tempEl.textContent = "Temp: " + data.daily[i].temp.day + "°F";
-    humidityEl.textContent = "Humidity: " + data.daily[i].humidty + "%";
-    windEl.textContent = "Wind Speed: " + data.daily[i].wind_speed + "mph";
+        dateEl.textContent = date.toLocaleDateString("en-US");
+        iconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + icon + "@2x.png");
+        tempEl.textContent = "Temp: " + data.daily[i].temp.day + "°F";
+        humidityEl.textContent = "Humidity: " + data.daily[i].humidity + "%";
+        windEl.textContent = "Wind Spd: " + data.daily[i].wind_speed + "mph";
 
-    forecastEl.classList.remoce("hidden");
+        forecastsEl.classList.remove("hidden");
     }
 }
 
-function saveHistory(input) {
-    console.log("Save History Success");
-    localStorage.setItem("history", input)
+//Saved History
+function saveHistory(city) {
+
+    if (!historyArray) {
+        historyArray = [city];
+    } else {
+        historyArray.unshift(city);
+    };
+    if (historyArray.length > 5) {
+        var delExtra = historyArray.pop();
+    }
+
+    localStorage.setItem("history", JSON.stringify(historyArray));
 }
 
+// Load 
+function loadHistory() {
+    var historyArray = JSON.parse(localStorage.getItem("history"));
+
+    return historyArray;
+}
+
+//Updated 
+function updateHistory() {
+    if (historyArray != null) {
+        for (var i = 0; i < historyArray.length; i++) {
+            var historyBtn = document.getElementById("btn" + i);
+
+            historyBtn.textContent = historyArray[i];
+
+            historyBtn.addEventListener("click", function() {convertHistorySearch(event.target)});
+        }
+    }
+}
+
+// Buttons
 searchBtn.addEventListener("click", convertSearch);
+input.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        document.getElementById("searchBtn").click();
+    }
+});
+
+// load all and update functions 
+loadHistory();
+updateHistory();
